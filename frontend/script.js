81a8c27d168b266fcbc65c2e15c20b6db83fc912
ytloe <531228931@qq.com>
2025-07-17 10:56:51 +0800

    use VITE

--- frontend/script.js
+++ frontend/script.js
@@ -13,8 +13,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // --- Supabase 配置 ---
   // ！！！替换成你自己的 Supabase 项目 URL 和 Anon Key ！！！
-  const SUPABASE_URL = "YOUR_SUPABASE_PROJECT_URL";
-  const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+  // const SUPABASE_URL = "YOUR_SUPABASE_PROJECT_URL";
+  // const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+  const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+  const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
   let supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
   // --- 应用状态变量 ---
